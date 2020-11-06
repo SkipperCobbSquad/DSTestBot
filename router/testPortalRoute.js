bab0b0dc22524f1e460d0a3ccfc51e44325b70bf
SzymonFaligowski <46247235+SzymonFaligowski@users.noreply.github.com>
2020-11-06 23:55:27 +0100

    Fin first phase

--- router/testPortalRoute.js
+++ router/testPortalRoute.js
@@ -32,46 +32,6 @@ const testportal = (msg, args) => {
       const ME = createME(result.quest, result?.answers, result.selector);
       msg.channel.send(ME);
     }
-    // const maxi = (table) => {
-    //   const questEm = new MessageEmbed()
-    //     .setColor('#0099ff')
-    //     .setTitle('Test Portal')
-    //     .attachFiles(['./Logo.png'])
-    //     .setAuthor('JarvisSystems', 'attachment://Logo.png')
-    //     .setTimestamp()
-    //     .setFooter('JarvisSystems', 'attachment://Logo.png');
-
-    //   if (table.length <= 25 && table.length > 0) {
-    //     let i = 1;
-    //     for (const quest of table) {
-    //       questEm.addField(
-    //         `${quest.quest}:  ${quest.multi ? 'multi' : 'single'}`,
-    //         quest.outAnsw(),
-    //         true
-    //       );
-    //     }
-    //     msg.channel.send(questEm);
-    //   } else {
-    //     const first = table.slice(0, 25);
-    //     const second = table.slice(26);
-    //     let i = 1;
-    //     for (const quest of first) {
-    //       questEm.addField(
-    //         `${quest.quest}:  ${quest?.multi ? 'multi' : 'single'}`,
-    //         quest.outAnsw(),
-    //         true
-    //       );
-    //     }
-    //     msg.channel.send(questEm);
-    //     maxi(second);
-    //   }
-    // };
-
-    // if (error) {
-    //   msg.channel.send('Something wrong with your URL (PROBABLY)');
-    // } else if (results.length) {
-    //   maxi(results);
-    // }
   })();
 };
 
